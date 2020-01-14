50ac83d8934f9fc1914c9fc3e1ac70b8befd2660
laetitia123 <uwizelaetitia@gmail.com>
2020-01-14 05:46:04 +0200

    first commit

---
+++ auth/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    # url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    # url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'login/',views.login_view,name='login'),
+    # url(r'CustomLoginView/',views.CustomLoginView,name='CustomLoginView'),
+    url(r'logout/',views.logout_view,name='logout'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
+]
